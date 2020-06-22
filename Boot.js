ea2634652ecfc1faca56da453edf54c497d321dd
Raul Ramirez <raaramirezpe@unal.edu.co>
2020-06-21 23:37:50 -0500

    add poewr and kill enemy

---
+++ Boot.js
@@ -0,0 +1,14 @@
+var Game = {};
+
+Game.Boot = function(game) {
+
+};
+
+Game.Boot.prototype = {
+    init: function() {
+        this.init.maxPointers = 1;
+        this.stage.disableVisivilityChange = true;
+    }
+
+
+}
--- game.js
+++ game.js
@@ -8,70 +8,144 @@ var game = new Phaser.Game(800, 800, Phaser.CANVAS, "container");
 var background;
 var sky;
 var player;
-var flechaderecha;
-var flechaizquierda;
+var rightButton;
+var leftBUtton;
 var jumpButton;
 var startImage = 0;
 var jumpTimer = 0;
 var direction = -1;
-
+var playerRelativeXPosition;
+var floor;
+var woofPower = null;
+var woofPowerSound;
 // Define "jugando", donde va a ocurrir el juego
 // El estado jugando tiene tres métodos básicos: preload, create y update
-var jugando = {
+var level1 = {
 
     preload: function() { // primero carga todas las imágenes
         game.load.image("imgfondo", "assets/background3.jpg");
         game.load.spritesheet("player", "assets/dogSequence2.png ", 125, 118);
+        game.load.image("woofwave", "assets/woofWave.png ", 125, 118);
+        game.load.image("enemy1", "assets/enemy2.png ", 125, 118);
+        game.load.audio("woofSound", "assets/woofWave.ogg");
     },
 
+
     create: function() { // crea los actores y muestra las imágenes
+
+        enemies = game.add.group();
+
         background = game.add.tileSprite(0, 0, 800, 800, "imgfondo");
+
         player = game.add.sprite(100, 325, "player");
         player.anchor.setTo(0.5); // centra las coordenadas del objeto
         player.animations.add("caminar", [0, 1, 2, 3, 4, 5, 6, 7], 10, true);
-        flechaderecha = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT); // activa la tecla derecha
-        flechaizquierda = game.input.keyboard.addKey(Phaser.Keyboard.LEFT); // activa la tecla izquierda
+
+        enemy = game.add.sprite(game.width + 100, game.height - 20, "enemy1")
+        enemy.anchor.setTo(0, 1);
+        playerRelativeXPosition = 100;
+
+        rightButton = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT); // activa la tecla derecha
+        leftBUtton = game.input.keyboard.addKey(Phaser.Keyboard.LEFT); // activa la tecla izquierda
         jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+        firePower = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
+
+        woofPowerSound = game.add.audio("woofSound");
+        //   game.sound.setDecodedCallback([woofPowerSound], update, this);
+
         game.physics.startSystem(Phaser.Physics.ARCADE); // activa la capacidad de reconocer coliciones, bordes, gravedad, etc.
         game.physics.arcade.enable(player);
-        player.body.collideWorldBounds = true; // no deja salir de los límites del juego
+        game.physics.arcade.enable(enemy);
 
+        player.body.collideWorldBounds = true; // no deja salir de los límites del juego
         player.body.bounce.y = 0.3
         player.body.gravity.y = 800
-        player.body.collideWorldBounds = true
+
     },
 
     update: function() { // ejecuta de manera reiterativa
+        //background.tilePosition.x += direction;
+
+        woofPowerSound.play();
         player.animations.play("caminar");
-        background.tilePosition.x += direction;
-        if (flechaderecha.isDown) {
-            direction = -1;
-            player.position.x += 2;
-            background.tilePosition.x -= 1;
-            player.scale.setTo(1, 1);
+        if (enemy.position.x > -200) {
+            enemy.position.x -= 1;
         }
-        if (flechaizquierda.isDown) {
-            direction = 1;
-            player.position.x -= 2;
-            player.scale.setTo(-1, 1);
-            background.tilePosition.x += 2;
+
+        if (woofPower != null) {
+            woofPower.position.x += 3;
+            killEnemy();
+        }
+
+        console.log("Enemy position ", enemy.position.x)
+        console.log("Player position ", playerRelativeXPosition)
+
+        samePosition();
+
+        if (rightButton.isDown) {
+            direction = -1;
+
+            player.position.x += 2;
+            playerRelativeXPosition += 2;
+            player.scale.setTo(1, 1);
+
+            background.tilePosition.x -= 1;
+            if (enemy.position.x > -200) {
+                enemy.position.x -= 2;
+            }
+        } else if (leftBUtton.isDown) {
+
+            if (playerRelativeXPosition > 150) {
+                direction = 1;
+                player.position.x -= 2;
+                playerRelativeXPosition -= 2;
+                player.scale.setTo(-1, 1);
+                background.tilePosition.x += 1;
+                if (enemy.position.x > -200) {
+                    enemy.position.x += 1;
+                }
+            }
+        } else {
+            player.animations.stop("caminar");
+        }
+
+        if (firePower.isDown) {
+            if (woofPower == null) {
+                woofPowerSound.play();
+                woofPower = game.add.sprite(playerRelativeXPosition + 10, game.height - 20, "woofwave");
+                woofPower.anchor.setTo(0, 1);
+            }
         }
         if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer) {
+            samePosition();
             player.body.velocity.y = -250;
             jumpTimer = game.time.now + 750;
         }
-        if (game.input.activePointer.isDown) { // si el puntero del mouse está undido ( o el dedo toca la pantalla)...
-            this.parar(); // ejecuta la función "parar()"
-        } else { //  si no está undido.....
-            player.angle = 0; //  devuelve el pescado a la rotación inicial 
-        }
     },
 
-    parar: function() { // detiene el fondo y rota el pescado 
-        player.angle -= 1; // gira el pescado un grado
-        background.tilePosition.x += 1; // contraresta el movimiento del fondo y lo detiene
-    },
+    render: function() {
+
+        game.debug.cameraInfo(game.camera, 32, 32);
+
+    }
 }
 
-game.state.add("activo", jugando); // define a "jugando" como un estado
-game.state.start("activo"); // inicia con el estado "jugando"
+function samePosition() {
+    if (enemy.position.x == playerRelativeXPosition || enemy.position.x - playerRelativeXPosition == 1 || playerRelativeXPosition - enemy.position.x == 1) {
+        console.log("Same position")
+        window.alert("Same position")
+
+    }
+}
+
+function killEnemy() {
+    if (enemy.position.x == woofPower.position.x || enemy.position.x - woofPower.position.x == 1 || woofPower.position.x - enemy.position.x == 1) {
+        woofPower.position.x = -200;
+        woofPower = null;
+        enemy.body.gravity.y = 800;
+        enemy.position.x = -200;
+    }
+}
+
+game.state.add("Level1", level1); // define a "jugando" como un estado1
+game.state.start("Level1"); // inicia con el estado "jugando" un estado
